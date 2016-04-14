0852732d3be43b5083b92c6032af08cb329c9578
Uladzimir Vysotski <Uladzimir.Vysotski@easyjet.com>
2016-04-14 14:09:45 +0300

    First task profiled and optimized

--- 1.ProfileSample/ProfileSample/Models/ImageModel.cs
+++ 1.ProfileSample/ProfileSample/Models/ImageModel.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections.Generic;
-
-namespace ProfileSample.Models
+﻿namespace ProfileSample.Models
 {
     public class ImageModel
     {
+        public ImageModel()
+        {
+            
+        }
+
+        public ImageModel(string name, byte[] data)
+        {
+            Name = name;
+            Data = data;
+        }
+
         public string Name { get; set; }
 
         public byte[] Data { get; set; } 
